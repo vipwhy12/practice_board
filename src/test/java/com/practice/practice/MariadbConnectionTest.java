78780e6b0a5d7a7500fc3ef4aa9ebd0eeeba5d55
Yuna <85014628+vipwhy12@users.noreply.github.com>
2022-05-31 20:36:45 +0900

    add user and password

--- src/test/java/com/practice/practice/MariadbConnectionTest.java
+++ src/test/java/com/practice/practice/MariadbConnectionTest.java
@@ -4,7 +4,7 @@ public class MariadbConnectionTest {
     // MySQL Connector 의 클래스. DB 연결 드라이버 정의
     private static final String DRIVER = "org.mariadb.jdbc.Driver";
     // DB 경로
-    private static final String URL = "jdbc:mariadb://aws rds 주소:3306/db이름";
-    private static final String USER = "username";
-    private static final String PASSWORD = "password";
+    private static final String URL = "jdbc:mariadb:localhost:3306/practice";
+    private static final String USER = "admin";
+    private static final String PASSWORD = "admin1234";
 }
