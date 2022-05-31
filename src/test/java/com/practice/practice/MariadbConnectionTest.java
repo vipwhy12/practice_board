becc712556e2c84a15d80667e892739870540cf5
Yuna <85014628+vipwhy12@users.noreply.github.com>
2022-05-31 20:34:23 +0900

    Initial commit

---
+++ src/test/java/com/practice/practice/MariadbConnectionTest.java
@@ -0,0 +1,10 @@
+package com.practice.practice;
+
+public class MariadbConnectionTest {
+    // MySQL Connector 의 클래스. DB 연결 드라이버 정의
+    private static final String DRIVER = "org.mariadb.jdbc.Driver";
+    // DB 경로
+    private static final String URL = "jdbc:mariadb://aws rds 주소:3306/db이름";
+    private static final String USER = "username";
+    private static final String PASSWORD = "password";
+}
