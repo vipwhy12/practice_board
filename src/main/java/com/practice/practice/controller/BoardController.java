4146b5eefb7faeb6c9daebd6c57cc2c4cfcbc484
Yuna <85014628+vipwhy12@users.noreply.github.com>
2022-07-05 23:29:31 +0900

    게시판 구현

--- src/main/java/com/practice/practice/controller/BoardController.java
+++ src/main/java/com/practice/practice/controller/BoardController.java
@@ -5,11 +5,14 @@ import com.practice.practice.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Slf4j
 @Controller
